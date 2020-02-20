aabcef05919ecf729da11df34664b72625358974
fegzycole <fergusoniyara@gmail.com>
2020-02-20 23:02:40 +0100

    Create module for non-existent ids

--- app/controllers/api/v1/todolists_controller.rb
+++ app/controllers/api/v1/todolists_controller.rb
@@ -1,4 +1,8 @@
+# frozen_string_literal: true
+
 class Api::V1::TodolistsController < ApplicationController
+  before_action :set_todolist, only: %i[update destroy]
+
   def create
     @todolist = Todolist.new(todo_params)
 
@@ -12,8 +16,6 @@ class Api::V1::TodolistsController < ApplicationController
   end
 
   def update
-    @todolist = Todolist.find(params[:id])
-
     @todolist.update(todo_update_params)
 
     respond_to do |format|
@@ -25,11 +27,31 @@ class Api::V1::TodolistsController < ApplicationController
     end
   end
 
+  def destroy
+    @todolist.destroy
+
+    respond_to do |format|
+      format.json { render json: { message: 'Todolist successfully deleted' }, status: :created }
+    end
+  end
+
+  private
+
   def todo_params
     params.require(:todolist).permit(:title, :user_id)
   end
 
   def todo_update_params
     params.require(:todolist).permit(:title)
+  end
+
+  def set_todolist
+    @todolist = Todolist.find(params[:id])
+
+    unless @todolist
+      respond_to do |format|
+        format.json { render json: { error: 'id does not exist' }, status: :created }
+      end
+    end
   end
 end
---
+++ app/controllers/concerns/exception_handler.rb
@@ -0,0 +1,12 @@
+module ExceptionHandler
+  # provides the more graceful `included` method
+  extend ActiveSupport::Concern
+
+  included do
+    rescue_from ActiveRecord::RecordNotFound do |e|
+      respond_to do |format|
+        format.json { render json: { error: e.message }, status: :not_found }
+      end
+    end
+  end
+end
